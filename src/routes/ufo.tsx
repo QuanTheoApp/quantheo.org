de8d8d6326f053e8817afa5e43b2a5bc3b47ba9b
QuanTheo <quantheoltd@gmail.com>
2026-06-16 13:53:52 +0700

    Increase image slideshow interval from 1000ms to 1500ms

--- src/routes/ufo.tsx
+++ src/routes/ufo.tsx
@@ -6,7 +6,7 @@ export const Route = createFileRoute('/ufo')({
 })
 
 const SLIDES = ['/01.jpg', '/02.jpg', '/04.jpg', '/04.jpg', '/05.jpg', '/06.jpg']
-const INTERVAL_MS = 1000
+const INTERVAL_MS = 1500
 
 function UFO() {
   const [current, setCurrent] = useState(0)
